7f6f737cec2a632ca9f97c093e33eac6a3a9146a
Conobi <Conobi@users.noreply.github.com>
2020-11-15 02:45:58 +0100

    Add language selector

--- translate.js
+++ translate.js
@@ -1,10 +1,13 @@
-var userLang = navigator.language || navigator.userLanguage;
+//var userLang = navigator.language || navigator.userLanguage;
 
-async function updateText(language) {
-  i18n = await $.i18n().load({
-    "en": "/politiscales/langs/static_en.json",
-    "fr": "/politiscales/langs/static_fr.json"
-  });
+const translations = {
+  "en": "/politiscales/langs/static_en.json",
+  "fr": "/politiscales/langs/static_fr.json"
+}
+
+async function updateText() {
+  language = localStorage.getItem("language")
+  i18n = await $.i18n().load(translations);
 
   $.i18n().locale = language;
 
@@ -15,6 +18,26 @@ async function updateText(language) {
   }
 }
 
+function setLanguage() {
+  localStorage.setItem("language", $('.language option:selected').val());
+  updateText();
+}
+
 $(function() {
-  updateText(userLang);
+  // We check if the language is not set and not included in the project
+  if(localStorage.getItem("language") == null && Object.keys(translations).includes(navigator.language || navigator.userLanguage) == false) {
+    userLang = localStorage.setItem("language", "en");
+    $('.language option[value=en]').attr('selected','selected');
+
+  // If the language is not set but included in our files
+  } else if (localStorage.getItem("language") == null) {
+    localStorage.setItem("language", navigator.language || navigator.userLanguage);
+    $('.language option[value='+ navigator.language || navigator.userLanguage +']').attr('selected','selected');
+
+  // The language is set
+  } else {
+    $('.language option[value='+ localStorage.getItem("language") +']').attr('selected','selected');
+  }
+  updateText();
+  $('.language').on('change keyup', setLanguage);
 });
